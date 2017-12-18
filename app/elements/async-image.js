56ac2f310cb13d8d6e7e7a73ca26dc78c03273bd
Pramjeet Ahlawat <ahlawat.pramjeet@gmail.com>
2017-12-18 18:28:47 +0530

    pinch to zoom feature done

--- app/elements/async-image.js
+++ app/elements/async-image.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { View, StyleSheet, Image, Alert } from "react-native";
+import PhotoView from "react-native-photo-view";
 
+import { fetchFile } from "../api";
 import { isImageFileType } from "../utils";
 
 import store from "../store";
@@ -9,24 +11,55 @@ import Text from "./text";
 const fileIcon = require("../images/ic_file.png");
 
 class AsyncImage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      imageSource: `data:image/jpeg;base64,`,
+      errorMsg: null
+    };
+  }
+
+  componentDidMount() {
+    // this.fetchImage();
+  }
+
+  fetchImage = async () => {
+    const { fileType, uri } = this.props;
+    if (!fileType || isImageFileType(fileType)) {
+      try {
+        this.setState({
+          isLoading: true,
+          errorMsg: null
+        });
+        const base64Data = await fetchFile(uri);
+        const base64Image = `data:image/jpeg;base64,${base64Data}`;
+        this.setState({
+          isLoading: false,
+          imageSource: base64Image
+        });
+      } catch (e) {
+        this.setState({
+          isLoading: false,
+          errorMsg: e.message
+        });
+      }
+    }
+  };
   render() {
-    if (this.props.fileType && !isImageFileType(this.props.fileType)) {
+    if (!this.props.fileType || isImageFileType(this.props.fileType)) {
+      const { isLoading, errorMsg, imageSource } = this.state;
       return (
-        <Image
-          style={[this.props.style, this.props.fileStyle]}
-          source={fileIcon}
+        <PhotoView
+          style={[styles.image, this.props.style]}
+          source={{ uri: this.props.uri }}
         />
       );
     } else {
       return (
         <Image
-          style={[styles.image, this.props.style]}
-          source={{
-            uri: this.props.uri,
-            headers: {
-              Authorization: store.getState().loggedInUser.authToken
-            }
-          }}
+          style={[this.props.style, this.props.fileStyle]}
+          source={fileIcon}
         />
       );
     }
