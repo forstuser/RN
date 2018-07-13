556364b6e1d7cbc68c59c9f5d934ba6288efd626
Pramjeet Ahlawat <pramjeetahlawat@Pramjeets-Air.Dlink>
2018-07-13 12:50:49 +0530

    ehome using new tab screen container

--- app/containers/ehome-screen/index.js
+++ app/containers/ehome-screen/index.js
@@ -11,9 +11,7 @@ import {
   Dimensions
 } from "react-native";
 import { connect } from "react-redux";
-import ScrollableTabView, {
-  DefaultTabBar
-} from "react-native-scrollable-tab-view";
+
 import Icon from "react-native-vector-icons/Ionicons";
 import I18n from "../../i18n";
 import ProductsList from "../../components/products-list";
@@ -28,8 +26,8 @@ import Analytics from "../../analytics";
 import { SCREENS, PRODUCT_TYPES } from "../../constants";
 import { colors, defaultStyles } from "../../theme";
 import FilterModal from "./filter-modal";
-import BlueGradientBG from "../../components/blue-gradient-bg";
 import SmallDot from "../../components/small-dot";
+import TabsScreenContainer from "../../components/tabs-screen-container";
 
 const ehomeIcon = require("../../images/ehome.png");
 
@@ -151,24 +149,12 @@ class EhomeScreen extends Component {
     const { activeTabIndex, tabs } = this.state;
 
     return (
-      <ScreenContainer style={styles.container}>
-        <View style={styles.header}>
-          <BlueGradientBG />
-          <View style={styles.headerUpperHalf}>
-            <View style={styles.ehomeIconWrapper}>
-              <Image
-                source={ehomeIcon}
-                style={styles.ehomeIcon}
-                resizeMode="contain"
-              />
-            </View>
-
-            <View style={{ flex: 1, paddingRight: 20 }}>
-              <Text weight="Medium" style={styles.title}>
-                eHome
-              </Text>
-            </View>
-
+      <TabsScreenContainer
+        iconSource={ehomeIcon}
+        title="eHome"
+        onTabChange={this.onTabChange}
+        headerRight={
+          <View style={{ flexDirection: "row" }}>
             <TouchableOpacity
               style={{}}
               onPress={() => {
@@ -191,43 +177,23 @@ class EhomeScreen extends Component {
               />
             </TouchableOpacity>
           </View>
-
-          <View style={styles.headerLowerHalf} />
-        </View>
-
-        <View style={{ marginTop: -50, flex: 1 }}>
-          <ScrollableTabView
-            locked={true}
-            onChangeTab={this.onTabChange}
-            renderTabBar={() => <DefaultTabBar />}
-            tabBarUnderlineStyle={{
-              backgroundColor: colors.mainBlue,
-              height: 2
-            }}
-            tabBarBackgroundColor="transparent"
-            tabBarTextStyle={{
-              fontSize: 14,
-              fontFamily: `Quicksand-Bold`,
-              color: "#fff"
-            }}
-          >
-            {tabs.map((tab, index) => (
-              <ProductsList
-                key={tab.type}
-                type={tab.type}
-                tabLabel={tab.name}
-                onRefresh={() => this.getProductsFirstPage(index)}
-                isLoadingFirstPage={tab.isLoadingFirstPage}
-                isLoading={tab.isLoading}
-                products={tab.products}
-                navigation={this.props.navigation}
-                error={tab.error}
-                onEndReached={() => this.getProducts(index)}
-                endHasReached={tab.endHasReached}
-              />
-            ))}
-          </ScrollableTabView>
-        </View>
+        }
+        tabs={tabs.map((tab, index) => (
+          <ProductsList
+            key={tab.type}
+            type={tab.type}
+            tabLabel={tab.name}
+            onRefresh={() => this.getProductsFirstPage(index)}
+            isLoadingFirstPage={tab.isLoadingFirstPage}
+            isLoading={tab.isLoading}
+            products={tab.products}
+            navigation={this.props.navigation}
+            error={tab.error}
+            onEndReached={() => this.getProducts(index)}
+            endHasReached={tab.endHasReached}
+          />
+        ))}
+      >
         <FilterModal
           ref={node => {
             this.filterModal = node;
@@ -235,83 +201,12 @@ class EhomeScreen extends Component {
           mainCategories={tabs[activeTabIndex].mainCategories}
           applyFilter={this.applyFilter}
         />
-      </ScreenContainer>
+      </TabsScreenContainer>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 0
-    // backgroundColor: colors.pinkishOrange
-  },
-  header: {
-    paddingBottom: 0,
-    width: "100%",
-    height: 120,
-    ...Platform.select({
-      ios: { height: 120, paddingTop: 20 },
-      android: { height: 100, paddingTop: 0 }
-    })
-  },
-  headerUpperHalf: {
-    paddingHorizontal: 16,
-    flexDirection: "row",
-    flex: 1,
-    alignItems: "center"
-  },
-  ehomeIconWrapper: {
-    width: 24,
-    height: 24,
-    padding: 2,
-    borderRadius: 2,
-    marginRight: 5
-  },
-  ehomeIcon: {
-    width: "100%",
-    height: "100%",
-    tintColor: "#fff",
-    marginRight: 5
-  },
-  title: {
-    fontSize: 18,
-    color: "#fff"
-  },
-
-  headerLowerHalf: {
-    height: 60,
-    flexDirection: "row"
-  },
-  tab: {
-    flex: 1,
-    height: 40,
-    alignItems: "center",
-    justifyContent: "center",
-    borderBottomWidth: 2,
-    borderBottomColor: "transparent"
-  },
-  tabUnderline: {
-    backgroundColor: colors.mainBlue,
-    height: 3,
-    width: SCREEN_WIDTH / 3,
-    position: "absolute",
-    left: 0,
-    bottom: 0
-  },
-  tabText: {
-    fontSize: 18,
-    color: "#fff"
-  },
-  pages: {
-    flex: 1,
-    width: SCREEN_WIDTH * 2,
-    flexDirection: "row"
-  },
-  page: {
-    width: SCREEN_WIDTH,
-    flexDirection: "row"
-  }
-});
+const styles = StyleSheet.create({});
 
 const mapStateToProps = state => {
   return {
