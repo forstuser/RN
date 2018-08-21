5583c05863e919b7a02a76271a3f1a8f48485cea
Pramjeet Ahlawat <pramjeetahlawat@Pramjeets-Air.Dlink>
2018-08-21 19:30:47 +0530

    my seller item ui changes

--- app/containers/my-sellers-screen/index.js
+++ app/containers/my-sellers-screen/index.js
@@ -13,7 +13,7 @@ import DrawerScreenContainer from "../../components/drawer-screen-container";
 
 import LoadingOverlay from "../../components/loading-overlay";
 import ErrorOverlay from "../../components/error-overlay";
-import { defaultStyles } from "../../theme";
+import { defaultStyles, colors } from "../../theme";
 import { SCREENS } from "../../constants";
 
 export default class MySellersScreen extends React.Component {
@@ -127,16 +127,103 @@ export default class MySellersScreen extends React.Component {
               return (
                 <TouchableOpacity
                   style={{
-                    height: 90,
                     ...defaultStyles.card,
                     margin: 10,
-                    borderRadius: 10,
-                    padding: 15,
-                    flexDirection: "row",
-                    alignItems: "center"
+                    borderRadius: 10
                   }}
                 >
-                  <Text weight="Bold">{item.name}</Text>
+                  <View
+                    style={{
+                      flexDirection: "row"
+                    }}
+                  >
+                    <View style={{ padding: 12 }}>
+                      <View style={{}}>
+                        <Image
+                          style={{
+                            width: 68,
+                            height: 68,
+                            borderRadius: 35,
+                            backgroundColor: "#eee"
+                          }}
+                        />
+                        <View
+                          style={{
+                            position: "absolute",
+                            right: 10,
+                            bottom: 0,
+                            width: 16,
+                            height: 16,
+                            borderRadius: 8,
+                            backgroundColor: colors.success,
+                            alignItems: "center",
+                            justifyContent: "center"
+                          }}
+                        >
+                          <Icon name="md-checkmark" color="#fff" size={12} />
+                        </View>
+                      </View>
+                    </View>
+                    <View style={{ padding: 12, paddingLeft: 0, flex: 1 }}>
+                      <View style={{ flexDirection: "row" }}>
+                        <View style={{ flex: 1, justifyContent: "center" }}>
+                          <Text weight="Bold" style={{ fontSize: 13 }}>
+                            {item.name}
+                          </Text>
+                          <Text style={{ fontSize: 11 }}>John Smith</Text>
+                        </View>
+                        <View
+                          style={{
+                            width: 42,
+                            height: 42
+                          }}
+                        >
+                          <Image
+                            style={{
+                              width: 42,
+                              height: 42,
+                              position: "absolute"
+                            }}
+                            source={require("../../images/offers_bg.png")}
+                          />
+                          <Text
+                            weight="Bold"
+                            style={{
+                              marginTop: 5,
+                              fontSize: 10,
+                              color: "#fff",
+                              textAlign: "center"
+                            }}
+                          >
+                            {`3\nOffers`}
+                          </Text>
+                        </View>
+                      </View>
+
+                      <View style={{ flexDirection: "row" }}>
+                        <Text style={{ fontSize: 11 }}>Credit Due : </Text>
+                        <Text style={{ fontSize: 11, color: colors.mainBlue }}>
+                          Rs. {item.credit_total}
+                        </Text>
+                      </View>
+                      <View style={{ flexDirection: "row" }}>
+                        <Text style={{ fontSize: 11 }}>Points Earned : </Text>
+                        <Text style={{ fontSize: 11, color: colors.mainBlue }}>
+                          {item.loyalty_total}
+                        </Text>
+                      </View>
+                    </View>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      height: 30,
+                      borderTopColor: "#d9d9d9",
+                      borderTopWidth: 1
+                    }}
+                  >
+                    <Text>Call</Text>
+                  </View>
                 </TouchableOpacity>
               );
             }}
