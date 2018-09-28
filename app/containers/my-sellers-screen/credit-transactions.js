07165fa2265743a939fc1e366409c8d65bef288f
cpugirl <pritam.kumar@binbill.com>
2018-09-28 15:35:35 +0530

    credit transaction screen fixed

--- app/containers/my-sellers-screen/credit-transactions.js
+++ app/containers/my-sellers-screen/credit-transactions.js
@@ -8,7 +8,7 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import StarRating from "react-native-star-rating";
-
+import moment from "moment";
 import { API_BASE_URL, getSellerDetails } from "../../api";
 
 import { Text, Image, Button } from "../../elements";
@@ -32,7 +32,7 @@ export default class MySellersCreditTransactionsScreen extends React.Component {
   };
 
   componentDidMount() {
-    // this.getSellerDetails();
+    this.getSellerDetails();
   }
 
   getSellerDetails = async () => {
@@ -55,11 +55,60 @@ export default class MySellersCreditTransactionsScreen extends React.Component {
 
   render() {
     const { navigation } = this.props;
-    const seller = navigation.getParam("seller", {});
+    const { transactions, isLoading } = this.state;
+    console.log("transaction is ", transactions);
+    // const seller = navigation.getParam("seller", {});
 
     return (
       <View style={{ flex: 1, backgroundColor: "#fff" }}>
-        <CreditTransactionsList sellerID={seller.id} />
+        <FlatList
+          style={{ flex: 1, marginBottom: 5 }}
+          data={transactions}
+          keyExtractor={item => item.id}
+          refreshing={isLoading}
+          onRefresh={this.getSellerDetails}
+          renderItem={({ item }) => {
+            return (
+              <View
+                style={{
+                  ...defaultStyles.card,
+                  margin: 10,
+                  borderRadius: 10,
+                  overflow: "hidden",
+                  flexDirection: "row",
+                  padding: 10
+                }}
+              >
+                <View style={{ alignItems: "center", paddingHorizontal: 10 }}>
+                  <Text
+                    weight="Bold"
+                    style={{ fontSize: 33, color: "#ababab", marginTop: -10 }}
+                  >
+                    {moment(item.created_at).format("DD")}
+                  </Text>
+                  <Text
+                    weight="Bold"
+                    style={{ fontSize: 15, color: "#ababab", marginTop: -5 }}
+                  >
+                    {moment(item.created_at).format("MMM")}
+                  </Text>
+                </View>
+                <View style={{ flex: 1, paddingHorizontal: 5 }}>
+                  <Text style={{ fontSize: 9 }}>
+                    Credit Added :<Text weight="Bold">{` ` + item.amount}</Text>
+                  </Text>
+                  <Text style={{ fontSize: 9, marginVertical: 5 }}>
+                    Transaction Id :
+                    <Text weight="Medium" style={{ color: colors.mainBlue }}>
+                      {` ` + item.id}
+                    </Text>
+                  </Text>
+                  <Text style={{ fontSize: 9 }}>{item.description}</Text>
+                </View>
+              </View>
+            );
+          }}
+        />
       </View>
     );
   }
