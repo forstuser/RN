31a915a6b90d8cdd6456912913846ea6761976c8
Pramjeet Ahlawat <pramjeetahlawat@Pramjeets-Air.Dlink>
2018-09-29 14:00:37 +0530

    notification fix

--- android/app/src/main/java/com/bin/binbillcustomer/MainActivity.java
+++ android/app/src/main/java/com/bin/binbillcustomer/MainActivity.java
@@ -63,6 +63,7 @@ public class MainActivity extends ReactActivity {
   @Override
   public void onNewIntent(Intent intent) {
     super.onNewIntent(intent);
+    setIntent(intent);
     handleIntent(intent);
   }
 
