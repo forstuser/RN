f0778ace489bf07e62e5fd01ebf1c4638a781d7b
cpugirl <pritam.kumar@binbill.com>
2018-11-19 20:11:06 +0530

    digital bill complete for offline payment

--- app/containers/more-screen/body.js
+++ app/containers/more-screen/body.js
@@ -162,10 +162,6 @@ class Body extends Component {
     this.closeDrawer();
     this.props.navigation.navigate(SCREENS.MY_ORDERS_SCREEN);
   };
-  onDigitalBillPress = () => {
-    this.closeDrawer();
-    this.props.navigation.navigate(SCREENS.DIGITAL_BILL_SCREEN);
-  };
   onCashbackBillsPress = () => {
     this.closeDrawer();
     this.props.navigation.navigate(SCREENS.CASHBACK_BILLS_SCREEN);
@@ -187,12 +183,6 @@ class Body extends Component {
 
     return (
       <ScrollView>
-        <MoreItem
-          onPress={this.onDigitalBillPress}
-          imageSource={require("../../images/orders_icon.png")}
-          text={"Digital Bill UI"}
-          imageStyle={{ width: 20, height: 20 }}
-        />
         <MoreItem
           onPress={this.onMyOrdersPress}
           imageSource={require("../../images/orders_icon.png")}
